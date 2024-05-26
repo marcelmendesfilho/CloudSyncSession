072ca869f58838536975d2a2738c92dae073b643
Marcel Mendes Filho <marcel.mendes@gmail.com>
2024-05-26 11:13:33 -0300

    feat: improving leave sharing, reducing complexity

--- Sources/CloudSyncSession/OperationHandler.swift
+++ Sources/CloudSyncSession/OperationHandler.swift
@@ -1,3 +1,5 @@
+import CloudKit
+
 public protocol OperationHandler {
     func handle(fetchOperation: FetchOperation, completion: @escaping (Result<FetchOperation.Response, Error>) -> Void)
     func handle(fetchRecordsOperation: FetchRecordsOperation, completion: @escaping (Result<FetchRecordsOperation.Response, Error>) -> Void)
@@ -5,5 +7,5 @@ public protocol OperationHandler {
     func handle(createZoneOperation: CreateZoneOperation, completion: @escaping (Result<Bool, Error>) -> Void)
     func handle(createSubscriptionOperation: CreateSubscriptionOperation, completion: @escaping (Result<Bool, Error>) -> Void)
     func handle(fetchShareParticipants: FetchShareParticipantsOperation, completion: @escaping (Result<FetchShareParticipantsOperation.Response, Error>) -> Void)
-    func leaveSharing(completion: @escaping (Result<Bool, Error>) -> Void)
+    func leaveSharing(shareRecord: CKShare, completion: @escaping (Result<Bool, Error>) -> Void)
 }
